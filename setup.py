859632ce79eb71626bac8e8fd2775e1c3a5ec9a5
Mueez Oso <osomueez7@gmail.com>
2026-04-23 13:24:39 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+
+    with open(file_path) as file_object:
+        requirements = file_object.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="EndToEnd",
+    version="0.0.1",
+    author="Mueez",
+    author_email="osomueez7@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
+
+
+
+
