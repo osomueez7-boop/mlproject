58ee7efb3b5252e279086c88f4d7b440a56ed059
Mueez Oso <osomueez7@gmail.com>
2026-04-24 01:47:40 +0100

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -70,8 +73,11 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_path, test_path = obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
     # print("Data ingestion completed successfully.")
     # print(f"Train file: {train_path}")
     # print(f"Test file: {test_path}")
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
